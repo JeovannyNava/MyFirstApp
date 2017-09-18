5eedcc0d80471d34c23627b40c1aa4089e29e6b8
GianniNava <jeovanny156@gmail.com>
2017-09-18 14:44:26 -0500

    Progress bar y toast. No es lo que me pediste pero es un avance

--- app/src/main/java/mundo/hola/gianninava/github/MainActivity.java
+++ app/src/main/java/mundo/hola/gianninava/github/MainActivity.java
@@ -1,56 +1,48 @@
 package mundo.hola.gianninava.github;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
+
+import android.content.Context;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity  {
-    ImageView imagen;
-    Button boton;
-    static final int REQUEST_IMAGE_CAPTURE=1;
+    private ProgressBar barra;
+    private Button boton;
+    private Context context;
+  ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       /* boton = (Button) findViewById(R.id.boton);
-        imagen = (ImageView) findViewById(R.id.imagen);
 
+        barra=(ProgressBar)findViewById(R.id.progressBar);
+        boton= (Button) findViewById(R.id.button);
+        final Context context = getApplicationContext();
 
-       boton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        boton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
 
-                llamarIntent();
+                barra.setVisibility(View.VISIBLE);
+
+                CharSequence text = "Iniciando Sesión";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
 
             }
-
-
         });
-    }
 
-    private void llamarIntent(){
-
-        Intent takePictureIntent= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) !=null){
-            startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE);
         }
-    }
 
-        @Override
-        protected void onActivityResult(int requestCode, int ResultCode, Intent data){
-            if (requestCode == REQUEST_IMAGE_CAPTURE && ResultCode == RESULT_OK){
 
-                Bundle extras = data.getExtras();
-                Bitmap imageBitmap = (Bitmap) extras.get("data");
-                imagen.setImageBitmap(imageBitmap);
-            }*/
-        }
+
 }
 
 
