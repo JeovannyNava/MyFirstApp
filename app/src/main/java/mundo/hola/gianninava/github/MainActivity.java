922c7492668bf7df793832a6f067ef88889bc78d
GianniNava <jeovanny156@gmail.com>
2017-10-03 01:02:19 -0500

    Avance de nuevo Login

--- app/src/main/java/mundo/hola/gianninava/github/MainActivity.java
+++ app/src/main/java/mundo/hola/gianninava/github/MainActivity.java
@@ -28,13 +28,14 @@ public class MainActivity extends AppCompatActivity {
     String test_email="email@gmail.com";
     String test_password="Contra123";*/
 
-
+    View decorView = getWindow().getDecorView();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
 
 
        /* View decorView = getWindow().getDecorView();
@@ -52,14 +53,22 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-
-    protected void setStatusBarTranslucent(boolean makeTranslucent) {
-        if (makeTranslucent) {
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        } else {
-            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         }
     }
+
+
+
     /*public void Dialog(View v) {
 
         correo = (EditText) findViewById(R.id.email);
