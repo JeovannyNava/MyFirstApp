8baf3c0a6e49e654b1a2d6f6f1e010b18c604db4
GianniNava <jeovanny156@gmail.com>
2017-10-03 19:08:09 -0500

    Avance de nuevo Login

--- app/src/main/java/mundo/hola/gianninava/github/MainActivity.java
+++ app/src/main/java/mundo/hola/gianninava/github/MainActivity.java
@@ -1,34 +1,25 @@
 package mundo.hola.gianninava.github;
 
 
-import android.app.Dialog;
-import android.content.DialogInterface;
-import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import android.graphics.Color;
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.widget.TextView;
+;
 
 
 public class MainActivity extends AppCompatActivity {
 
 
-   /* private EditText correo; //Variables tipo EditText
-    private EditText password;
 
 
-    String c;
-    String p;
-    String test_email="email@gmail.com";
-    String test_password="Contra123";*/
-
-    View decorView = getWindow().getDecorView();
+    TextView textView;
+    String politicas="By signing up, I agree to Airbnb's Terms of Service, Pryvacy Policy, Guest Refund Policy, and Host Guarantee Terms. ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,165 +29,17 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-       /* View decorView = getWindow().getDecorView();
-// Hide both the navigation bar and the status bar.
-// SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
-// a general rule, you should design your app to hide the status bar whenever you
-// hide the navigation bar.
-        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);*/
+        textView = (TextView)findViewById(R.id.textView);
+        textView.setText(politicas);
+        SpannableString spannableString=new SpannableString(politicas);
 
-
-
-
-
+        spannableString.setSpan(new ForegroundColorSpan(Color.RED),34,51,0);
+        spannableString.setSpan(new ForegroundColorSpan(Color.RED),52,67,0);
+        spannableString.setSpan(new ForegroundColorSpan(Color.RED),68,88,0);
+        spannableString.setSpan(new ForegroundColorSpan(Color.RED),93,114,0);
+        textView.setText(spannableString);
 
     }
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if (hasFocus) {
-            decorView.setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        }
-    }
-
-
-
-    /*public void Dialog(View v) {
-
-        correo = (EditText) findViewById(R.id.email);
-        password = (EditText) findViewById(R.id.password);
-        c=correo.getText().toString();
-        p=password.getText().toString();
-
-
-        if(c.isEmpty() || p.isEmpty()){
-
-            Toast.makeText(MainActivity.this, "No hay datos", Toast.LENGTH_SHORT).show();
-
-        }
-
-        else {
-
-
-
-          if (valid()) {//Solo entra aqui si el formato de los datos son validos
-
-
-                if(c.equals(test_email) && p.equals(test_password)) {
-
-
-                    AlertDialog.Builder ventana = new AlertDialog.Builder(this);
-                    ventana.setMessage("Los datos son correctos");
-                    ventana.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            correo.setText("");
-                            password.setText("");
-                        }
-                    });
-                    ventana.show();
-                }
-
-                else if(c.equals(test_email) && !p.equals(test_email)){
-
-
-                    AlertDialog.Builder ventana = new AlertDialog.Builder(this);
-                    ventana.setMessage("Password incorrecto");
-                    ventana.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            correo.setText("");
-                            password.setText("");
-                        }
-                    });
-                    ventana.show();
-                }
-
-                else if(!c.equals(test_email) && p.equals(test_password)){
-
-
-                    AlertDialog.Builder ventana = new AlertDialog.Builder(this);
-                    ventana.setMessage("Email incorrecto");
-                    ventana.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            correo.setText("");
-                            password.setText("");
-                        }
-                    });
-                    ventana.show();
-
-              }
-
-                else if(!c.equals(test_email) && !p.equals(test_password)){
-
-
-                    AlertDialog.Builder ventana = new AlertDialog.Builder(this);
-                    ventana.setMessage("Email y password incorrectos");
-                    ventana.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            correo.setText("");
-                            password.setText("");
-                        }
-                    });
-                    ventana.show();
-                }
-
-            }
-        }
-
-
-
-
-    }
-
-    public boolean valid() {
-
-
-        Pattern p_c = Pattern.compile("^([0-9a-zA-Z]([_.w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-w]*[0-9a-zA-Z].)+([a-zA-Z]{2,9}.)+[a-zA-Z]{2,3})$");
-        Matcher m_c = p_c.matcher(c);
-        boolean b_c = m_c.find();
-
-        Pattern p_p = Pattern.compile("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{8,15}[^'\\s]/");
-        Matcher m_p = p_p.matcher(p);
-        boolean b_p = m_p.find();
-
-        if (b_c  && b_p ) {
-
-            Toast.makeText(MainActivity.this, "Datos validos", Toast.LENGTH_SHORT).show();
-
-
-        } else if(!b_c && b_p) {
-
-
-            Toast.makeText(MainActivity.this, "Correo no Valido", Toast.LENGTH_SHORT).show();
-        }
-
-        else if(b_c && !b_p){
-
-            Toast.makeText(MainActivity.this, "Password invalido", Toast.LENGTH_SHORT).show();
-       }
-
-       else if(!b_c && !b_p){
-
-            Toast.makeText(MainActivity.this,"Correo y Password no tienen un formato valido", Toast.LENGTH_SHORT).show();
-
-        }
-
-
-        return b_c & b_p;
-    }
-*/
-
 
 
 }
